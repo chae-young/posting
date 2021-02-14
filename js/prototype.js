2c6cdace1d1da754ef6f6f2fe8e34a44d7da4fa0
chae-young <cycy8527@gmail.com>
2021-02-14 14:54:21 +0900

    예제추가

--- js/prototype.js
+++ js/prototype.js
@@ -19,20 +19,31 @@
         this.job = 'siger'
     }
 
-    Person.prototype.age = 12;
+    Person.prototype.age = 13;
 
     var obj1 = new Person();
     var obj2 = new Person();
 
-    console.log(obj1.age)
-    console.log(obj2.age)
+    // console.log(obj1.age)
+    // console.log(obj2.age)
 
     
-    console.log(typeof Person.prototype) // object
-    console.log(typeof obj1.prototype) // undefined
-    console.log(typeof obj1.__proto__) //object
-    console.log(Person.prototype ===  obj1.__proto__) // true
+    // console.log(typeof Person.prototype) // object
+    // console.log(typeof obj1.prototype) // undefined
+    // console.log(typeof obj1.__proto__) //object
+    // console.log(Person.prototype ===  obj1.__proto__) // true
 
     var proto1 = Object.getPrototypeOf(obj1);
     
-    console.log(proto1.age)
+    // console.log(proto1.age)
+
+
+/* 프로토타입 1 - 예제3 */
+
+Person.prototype = {age:20}
+var obj3 = new Person();
+Person.prototype.alert = function(){
+    console.log('야호')
+}
+console.log(obj3)
+console.log(Person.prototype)
